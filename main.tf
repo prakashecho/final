725f3b6d91abc1420d2c74bef0c379fb18ce2af5
prakashecho <50212384+prakashecho@users.noreply.github.com>
2024-05-31 18:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,42 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "target_regions" {
-  type    = list(string)
-  default = ["us-east-1", "us-east-2", "us-west-1"]
-}
+# Output from Packer workflow
+variable "packer_ami_id" {}
 
-data "aws_ami" "source_ami" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["Jenkins-AMI"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_ami_copy" "encrypted_ami_copies" {
-  count             = length(var.target_regions)
-  name              = "encrypted-ami-${var.target_regions[count.index]}"
-  source_ami_id     = data.aws_ami.source_ami.id
+# Copy the source AMI and encrypt it using the existing KMS key
+resource "aws_ami_copy" "encrypted_ami1" {
+  name              = "encrypted-ami1"
+  source_ami_id     = var.packer_ami_id
   source_ami_region = "us-east-1"
   encrypted         = true
   kms_key_id        = "arn:aws:kms:us-east-1:874599947932:key/22ad3ccd-28a1-4d05-ad73-5f284cea93b3"
-  region            = var.target_regions[count.index]
 }
 
-resource "aws_ami_launch_permission" "shared_ami" {
-  count      = length(var.target_regions)
+output "new_ami_id" {
+  value = aws_ami_copy.encrypted_ami1.id
+}
+
+# Update the policy of the existing KMS key to allow usage by another AWS account
+# This part remains the same as your original code
+
+# Share the AMI with the specified account
+resource "aws_ami_launch_permission" "share_ami" {
+  image_id   = aws_ami_copy.encrypted_ami1.id
   account_id = "280435798514"
-  image_id   = aws_ami_copy.encrypted_ami_copies[count.index].id
 }
 
-resource "aws_kms_key_policy" "key_policy" {
-  key_id = "arn:aws:kms:us-east-1:874599947932:key/22ad3ccd-28a1-4d05-ad73-5f284cea93b3"
-  policy = jsonencode({
-    # ... (Your existing policy)
-  })
-}
+# Fetch the most recent EBS snapshot related to the newly created encrypted AMI
+# This part remains the same as your original code
+
+# Share the snapshot with the specified account
+# This part remains the same as your original code
