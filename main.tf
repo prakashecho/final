b5408890911d38764300e3452671cfe251665c39
prakashecho <50212384+prakashecho@users.noreply.github.com>
2024-05-31 20:13:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Output from Packer workflow
-variable "packer_ami_id" {}
+# Define the variable for the Packer AMI name
+variable "packer_ami_name" {
+  description = "The AMI name from the Packer build"
+  type        = string
+}
+
+# Data source to find the AMI ID by its name
+data "aws_ami" "packer_ami" {
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = [var.packer_ami_name]
+  }
+}
 
 # Copy the source AMI and encrypt it using the existing KMS key
 resource "aws_ami_copy" "encrypted_ami1" {
   name              = "encrypted-ami1"
-  source_ami_id     = var.packer_ami_id
+  source_ami_id     = data.aws_ami.packer_ami.id
   source_ami_region = "us-east-1"
   encrypted         = true
   kms_key_id        = "arn:aws:kms:us-east-1:874599947932:key/22ad3ccd-28a1-4d05-ad73-5f284cea93b3"
@@ -19,7 +32,54 @@ output "new_ami_id" {
 }
 
 # Update the policy of the existing KMS key to allow usage by another AWS account
-# This part remains the same as your original code
+resource "aws_kms_key_policy" "existing_key_policy" {
+  key_id = aws_ami_copy.encrypted_ami1.kms_key_id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "key-default-1",
+  "Statement": [
+    {
+      "Sid": "Enable IAM User Permissions",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::874599947932:root"
+      },
+      "Action": "kms:*",
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow use of the key",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::280435798514:root"
+      },
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow attachment of persistent resources",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::280435798514:root"
+      },
+      "Action": [
+        "kms:CreateGrant",
+        "kms:ListGrants",
+        "kms:RevokeGrant"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
 # Share the AMI with the specified account
 resource "aws_ami_launch_permission" "share_ami" {
@@ -28,7 +88,16 @@ resource "aws_ami_launch_permission" "share_ami" {
 }
 
 # Fetch the most recent EBS snapshot related to the newly created encrypted AMI
-# This part remains the same as your original code
+data "aws_ebs_snapshot" "snapshot" {
+  most_recent = true
+  filter {
+    name   = "description"
+    values = ["*${aws_ami_copy.encrypted_ami1.id}*"]
+  }
+}
 
 # Share the snapshot with the specified account
-# This part remains the same as your original code
+resource "aws_ebs_snapshot_permission" "share_snapshot" {
+  snapshot_id = data.aws_ebs_snapshot.snapshot.id
+  account_id  = "280435798514"
+}
